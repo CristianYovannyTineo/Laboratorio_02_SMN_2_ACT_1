58c38ee59c5283f8d61f9d44b7d6c7c24116edc9
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-08-28 23:48:25 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_02_SMN_2_ACT_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Se desea realizar un programa en C#. Que calcule el mayor de 3 numeros enteros ingresador por teclado.
+
+int numero1, numero2, numero3;
+
+// ingresar los numeros
+
+Console.WriteLine("Ingrese el numero");
+numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el numero");
+numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el numero");
+numero3 = int.Parse(Console.ReadLine());
+
+if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+{
+    int numeromayor = numero1;
+
+    if (numero2 > numero1)
+    {
+        numeromayor = numero2;
+    }
+    if (numero3 > numeromayor) { numeromayor = numero3; }
+
+    Console.WriteLine("El numero mayor es: " + numeromayor);
+}
+else
+{
+    Console.WriteLine("Los numeros no deben de ser iguales");
+}
